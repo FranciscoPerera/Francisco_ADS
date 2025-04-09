25ea49afe9f4e717b5c27a70e96b33751ae71372
FranciscoPerera <franciscopneto2017@gmail.com>
2025-04-09 15:32:04 -0300

    Add files via upload

---
+++ Exercicio/C/Ex_02.c
@@ -0,0 +1,5 @@
+// O banco do brasil está com uma campanha para incentivar as pessoas a usarem seu cartão de crédito. De acordo com o valor da fatura no mês, um certo percentual de 
+// cashback será creditado na conta corrente do usuário. Se o valor da fatura for de até R$1000,00, não haverá cashback. Faturas entre R$1000,00 e R$4000,00 terão 
+// cashback de 1%. Faturas entre R$4000,00 e R$8000,00 terão cashback de 2%. Faturas acima de R$8000,00 terão cashback de 5%. Faça um software em que o usuário 
+// informe o valor de sua fatura e você retornará na tela para ele, qual o valor de cashback que ele terá naquele mês.
+
---
+++ Exercicio/Python/Ex_02.py
@@ -0,0 +1,21 @@
+# O banco do brasil está com uma campanha para incentivar as pessoas a usarem seu cartão de crédito. De acordo com o valor da fatura no mês, um certo percentual de 
+# cashback será creditado na conta corrente do usuário. Se o valor da fatura for de até R$1000,00, não haverá cashback. Faturas entre R$1000,00 e R$4000,00 terão 
+# cashback de 1%. Faturas entre R$4000,00 e R$8000,00 terão cashback de 2%. Faturas acima de R$8000,00 terão cashback de 5%. Faça um software em que o usuário 
+# informe o valor de sua fatura e você retornará na tela para ele, qual o valor de cashback que ele terá naquele mês.
+
+print("------------Calculadora de cashback--------------")
+valor = float(input("Qual o valor de sua fatura: "))
+
+def calcular_cas(valor):
+    if valor <= 1000:
+        print("Não haverá cashback")
+        return 0
+    elif valor <= 4000:
+        return valor * 0.01
+    elif valor <= 8000:
+        return valor * 0.02
+    else:
+        return valor * 0.05
+
+cashback = calcular_cas(valor)
+print(f"Você terá R${cashback:.2f} de cashback")
