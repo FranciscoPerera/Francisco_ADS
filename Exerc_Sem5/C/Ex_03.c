4110fa526ac9089dd4461cd0c367c89b332c1be1
FranciscoPerera <franciscopneto2017@gmail.com>
2025-04-11 20:01:35 -0300

    Delete Exerc_Sem5 directory

--- Exerc_Sem5/C/Ex_03.c
+++
@@ -1,3 +0,0 @@
-// O supermercado Delta está fazendo uma promoção. Se a pessoa for sócio do Clube Delta ou ela tiver mais de 60 anos, terá R$10,00 de desconto no valor total da compra. 
-// Além disso, para os sócio do Clube Delta que estão fazendo aniversário naquele dia, haverá um desconto adicional de R$ 5,00. Esses descontos todos são validos 
-// somente para compras acima de R$ 100,00. Faça o melhor software possível, que receba as informações necessárias do usuário e ao final diga o valor total da compra.
