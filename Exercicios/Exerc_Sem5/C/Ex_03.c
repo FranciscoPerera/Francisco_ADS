3c51b222c8bf0d29da1ffd70458ca7c1ef993090
FranciscoPerera <franciscopneto2017@gmail.com>
2025-04-12 00:53:29 +0000

    alt

--- Exercicios/Exerc_Sem5/C/Ex_03.c
+++ Exercicios/Exerc_Sem5/C/Ex_03.c
@@ -1,7 +1,6 @@
 // O supermercado Delta está fazendo uma promoção. Se a pessoa for sócio do Clube Delta ou ela tiver mais de 60 anos, terá R$10,00 de desconto no valor total da compra. 
 // Além disso, para os sócio do Clube Delta que estão fazendo aniversário naquele dia, haverá um desconto adicional de R$ 5,00. Esses descontos todos são validos 
 // somente para compras acima de R$ 100,00. Faça o melhor software possível, que receba as informações necessárias do usuário e ao final diga o valor total da compra.
-
 #include <stdio.h>
 #include <string.h>
 
@@ -23,13 +22,12 @@ int main() {
     if (valor < 100) {
         printf("Valor total da compra: R$%.2f\n", valor);
     } else {
-        if (strcmp(socio, "1") == 0 && strcmp(niver, "1") == 0) {
+        if ((socio, "1") && (niver, "1") ) {
             valor -= 15; 
-        } else if (strcmp(socio, "1") == 0 || idade >= 60) {
+        } else if ((socio, "1") || idade >= 60) {
             valor -= 10; 
         }
         printf("Valor total da compra: R$%.2f\n", valor);
     }
 
-    return 0;
 }
