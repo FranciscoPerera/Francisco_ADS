cbdd3850a29c955611f16d501f63ea1ec7547c51
FranciscoPerera <franciscopneto2017@gmail.com>
2025-04-15 00:32:56 +0000

    alt

--- Exercicios/Exerc_sem6/imc.py
+++ Exercicios/Exerc_sem6/imc.py
@@ -1,21 +1,22 @@
 sair = 10
 while(sair != "0"):
+    print("--------------Calculador de IMC----------")
     peso = float(input("Digite seu peso(kg): "))
-    altura = float(input("Digite sua altura(m): "))
+    altura = float(input("Digite sua altura em metros(Ex.1.80): "))
     imc = (peso/(altura**2))
 
     if(imc<18.5):
-        print(f"Seu IMC é {imc}. Você está abaixo do peso")
+        print(f"Seu IMC é {imc:.2f}. Você está abaixo do peso")
     elif (imc<=24.9):
-        print(f"Seu IMC é {imc}. Você está no peso normal.")
+        print(f"Seu IMC é {imc:.2f}. Você está no peso normal.")
     elif (imc<=29.9):
-        print(f"Seu IMC é {imc}. Você está em sobrepeso.")
+        print(f"Seu IMC é {imc:.2f}. Você está em sobrepeso.")
     elif (imc<=39.9):
-        print(f"Seu IMC é {imc}. Você está em obesidade II.")
+        print(f"Seu IMC é {imc:.2f}. Você está em obesidade II.")
     elif (imc>=40):
-        print(f"Seu IMC é {imc}. Você está em obesidade III.")
+        print(f"Seu IMC é {imc:.2f}. Você está em obesidade III.")
     else:
         print("Dados de entrada incorretos")
 
-    sair = input("Digite 0 pra finanlizar. Ou clique em algo para continuar")
+    sair = input("Digite 0 pra finanlizar. Ou clique em algo para continuar.  ")
 
