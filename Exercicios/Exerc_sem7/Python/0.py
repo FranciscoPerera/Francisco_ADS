8b82cee603459f7974b1eac67ea67fe62e42b93b
FranciscoPerera <franciscopneto2017@gmail.com>
2025-04-25 19:48:46 +0000

    alt

--- Exercicios/Exerc_sem7/Python/0.py
+++ Exercicios/Exerc_sem7/Python/0.py
@@ -5,5 +5,5 @@ soma = 0
 for i in range(1, 501):
     if i % 3 == 0 and i % 2 != 0: # verifica se i é divisível por 3 e se i é ímpar.
         soma += i 
-
-print(f"A soma de todos os números ímpares múltiplos de 3 entre 1 e 500 é: {soma}")
+    
+print(f"A soma dos números ímpares múltiplos de 3 entre 1 e 500 é: {soma}")
