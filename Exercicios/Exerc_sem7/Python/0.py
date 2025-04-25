79b4c6fba6bd5b105be4ab7a285d77bd3e12ecde
FranciscoPerera <franciscopneto2017@gmail.com>
2025-04-25 19:43:06 +0000

    alt

--- Exercicios/Exerc_sem7/Python/0.py
+++ Exercicios/Exerc_sem7/Python/0.py
@@ -1,3 +1,9 @@
 # 0) Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de três e 
 # que se encontram no conjunto dos números de 1 até 500.
 
+soma = 0  # Variável acumuladora
+for i in range(1, 501):
+    if i % 3 == 0 and i % 2 != 0:
+        soma += i 
+
+print(f"A soma de todos os números ímpares múltiplos de 3 entre 1 e 500 é: {soma}")
