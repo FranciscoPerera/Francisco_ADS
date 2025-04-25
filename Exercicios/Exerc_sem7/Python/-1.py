a1b8ea0185debb8467d6ff06c1ddd58e3033eb16
FranciscoPerera <franciscopneto2017@gmail.com>
2025-04-25 18:57:06 +0000

    alt

---
+++ Exercicios/Exerc_sem7/Python/-1.py
@@ -0,0 +1,2 @@
+# -1) Faça um algoritmo que conte de 1 a 100 e a cada múltiplo de 10 emita uma mensagem: “Múltiplo de 10”.
+
---
+++ Exercicios/Exerc_sem7/Python/1.py
@@ -0,0 +1,2 @@
+# 1) Escreva um algoritmo que receba 7 valores numéricos e retorne na tela a mensagem:
+# “Entre os 7 valores, X são números negativos!”.
