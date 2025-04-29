5ecf30433e23383e088d9ef0b4b059146537530f
FranciscoPerera <franciscopneto2017@gmail.com>
2025-04-29 18:19:32 +0000

    alt

--- Reg_Ponto/Ponto_Py/ponto.py
+++ Reg_Ponto/Ponto_Py/ponto.py
@@ -7,26 +7,9 @@ codigo = {
     "05": "Ana"
 }
 
-# Cria a tabela de Registros de ponto
-registros = {
-    "01": [],
-    "02": [],
-    "03": [],
-    "04": [],
-    "05": []
-}
-
 # Função para verificar o usuário
 def verifica_usuario(cod):
     return codigo.get(cod, None)
-
-# Função para adicionar o ponto
-def adicionar_ponto(cod, tipo):
-    if cod in registros:
-        registros[cod].append(tipo)
-        print(f"Registro de ponto '{tipo}' adicionado para o usuário {codigo[cod]}")
-    else:
-        print("Código inválido!")
 
 print("-----------------Folha Ponto------------------")
 usuario = None
