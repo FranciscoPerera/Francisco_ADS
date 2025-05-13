5cfaf180c8a8d3f0483abd0c2419f316e17c9275
FranciscoPerera <franciscopneto2017@gmail.com>
2025-05-13 17:59:21 +0000

    alt

--- Reposição/Rep_0703/Python/3.py
+++ Reposição/Rep_0703/Python/3.py
@@ -1,5 +1,6 @@
 # 3) Refaça o programa anterior de forma que ao invés de considerar somente 1 semana, sejam armazenadas as temperaturas da semana 1, 2 e 3 do mês. 
 # Dessa vez, peça para o usuário inserir os valores medidos em cada dia de cada semana.
+
 import statistics
 
 dias_da_semana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
