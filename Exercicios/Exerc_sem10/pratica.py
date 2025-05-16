0cd57801e92e30a9ba7e5c01754ab292b9ef3cfa
FranciscoPerera <franciscopneto2017@gmail.com>
2025-05-16 16:14:38 +0000

    alt

--- Exercicios/Exerc_sem10/pratica.py
+++ Exercicios/Exerc_sem10/pratica.py
@@ -1,24 +1,25 @@
 # Lista sobre algoritmos
 
 lista = []
-sair = 1
-while(sair != "0"):
+sair = ""
+
+while sair != "0":
     print("-------------- Algoritmos Em Redes Sociais ----------")
     
-    empresas = input("Digite empresas que usam algoritmos para te atarir: ")
-    algoritmos.append(empresas)
-    dono_empresa = input("Quem é o dono dessa empresa: ")
-    algoritmos.append(dono_empresa)
-    gosta = input("Voce gosta dessa empresa: ")
-    algoritmos.append(gosta)
+    empresa = input("Digite uma empresa que usa algoritmos para te atrair: ")
+    dono_empresa = input("Quem é o dono dessa empresa? ")
+    gosta = input("Você gosta dessa empresa? ")
 
     algoritmos = {
-    "Empresa": empresas,
-    "DonoEmpresa": dono_empresa,
-    "VoceGosta": gosta
+        "Empresa": empresa,
+        "DonoEmpresa": dono_empresa,
+        "VoceGosta": gosta
     }
 
     lista.append(algoritmos)
-    sair = input("Digite 0 pra finanlizar. Ou clique em algo para continuar. ")
-else:
-    print("Saindo...")
+
+    sair = input("Digite 0 para finalizar ou qualquer outra tecla para continuar: ")
+
+print("\n---------- Lista final: ---------------")
+for item in lista:
+    print(item)
