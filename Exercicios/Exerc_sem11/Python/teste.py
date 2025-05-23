a4ab2eb36a919d0d09a1eac81706fe3517d585d5
FranciscoPerera <franciscopneto2017@gmail.com>
2025-05-23 17:32:41 +0000

    alt

---
+++ Exercicios/Exerc_sem11/Python/teste.py
@@ -0,0 +1,3 @@
+# Refaça o exercício de ordenação que fizemos no início da aula, porém,
+# utilizando uma função específica pra fazer a troca dos números quando necessário!
+
---
+++ Exercicios/Exerc_sem11/Python/teste_2.py
@@ -0,0 +1,8 @@
+# Uma das primeiras criptografias que surgiram foi a criptografia de cesar! Nela, existe uma chave e um 
+# valor a ser criptografado e descriptografado. Suponha que se deseja criptografar o número 16 
+# com a chave 3. Esse número criptografado será 19 (16+3). Ao descriptografar, a
+# pessoa que recebe, conhecendo a chave, irá subtrair seu valor e então perceberá que o número é 16! 
+# Crie em python um programa que receba um conjunto de números e um valor de chave. Pergunte se o 
+# usuário quer criptografar ou descriptografar. Faça a operação e retorne na tela o resultado. 
+# Detalhe: Crie ao menos 3 funções: criptografa() edescriptografa(). 
+
